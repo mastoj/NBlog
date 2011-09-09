df6c1dc71086568dece19dac1dd3b3263c5e7f0b
Tomas Jansson <tomas.jansson@gmail.com>
2011-09-10 01:02:10 +0200

    possible to navigate to index

---
+++ NBlog.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NBlog.Controllers;
+using NUnit.Framework;
+
+namespace NBlog.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void IndexAction_ShouldReturnIndexView()
+        {
+            // arrange
+            var homeController = new HomeController();
+
+            // act
+            var result = homeController.Index();
+
+            // assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
