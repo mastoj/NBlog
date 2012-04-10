f998618213b418431b3ace801f23631ee1ea6fae
Tomas Jansson <tomas.jansson@gmail.com>
2012-04-10 23:23:38 +0200

    added test for creating a second post

--- NBlog.Domain.Tests/Post/CreatePost.cs
+++ NBlog.Domain.Tests/Post/CreatePost.cs
@@ -25,9 +25,9 @@ namespace NBlog.Domain.Tests.Post.Create
         }
 
         [Test]
-        public void A_Create_Post_Event_Should_Be_Published()
+        public void The_Post_Should_Be_Created()
         {
-            var createPostEvent = GetPublishedEvents().First() as CreatePostEvent;
+            var createPostEvent = GetPublishedEvents().Last() as CreatePostEvent;
             createPostEvent.Content.Should().Be(_content);
             createPostEvent.EventNumber.Should().Be(0);
             createPostEvent.Excerpt.Should().Be(_excerpt);
@@ -45,7 +45,8 @@ namespace NBlog.Domain.Tests.Post.Create
         private DateTime _lowestPossibleDate;
     }
 
-    public class When_Creating_A_Post_But_Id_Is_Take : BaseCommandTest<CreatePostCommand>
+    [TestFixture]
+    public class When_Creating_A_Post_But_Id_Is_Taken : BaseCommandTest<CreatePostCommand>
     {
         protected override void Given()
         {
@@ -62,5 +63,46 @@ namespace NBlog.Domain.Tests.Post.Create
         {
             CaughtException.Should().BeOfType<DuplicatePostIdException>();
         }
+    }
+
+    [TestFixture]
+    public class When_Creating_A_Second_Post : BaseCommandTest<CreatePostCommand>
+    {
+        protected override void Given()
+        {
+            PreSetCommand(new CreatePostCommand("title", "content", "slug", null, "excerpt", Guid.Empty));
+        }
+
+        protected override CreatePostCommand When()
+        {
+            _lowestPossibleDate = DateTime.Now;
+            _title = "Title";
+            _content = "content";
+            _slug = "slug";
+            _tags = new List<string>() { "tag1", "tag2" };
+            _excerpt = "excerpt";
+            var createPostCommand = new CreatePostCommand(_title, _content, _slug, _tags, _excerpt, Guid.NewGuid());
+            return createPostCommand;
+        }
+
+        [Test]
+        public void The_Post_Should_Be_Created()
+        {
+            var createPostEvent = GetPublishedEvents().Last() as CreatePostEvent;
+            createPostEvent.Content.Should().Be(_content);
+            createPostEvent.EventNumber.Should().Be(0);
+            createPostEvent.Excerpt.Should().Be(_excerpt);
+            createPostEvent.Slug.Should().Be(_slug);
+            createPostEvent.Tags.SequenceEqual(_tags);
+            createPostEvent.Title.Should().Be(_title);
+            createPostEvent.CreationDate.Should().BeOnOrAfter(_lowestPossibleDate);
+        }
+
+        private string _title;
+        private string _content;
+        private string _slug;
+        private List<string> _tags;
+        private string _excerpt;
+        private DateTime _lowestPossibleDate;
     }
 }
