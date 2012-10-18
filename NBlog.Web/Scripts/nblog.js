253b024f7499b8be0692ca3e9d59b41d292be9bd
Tomas Jansson <tomas.jansson@gmail.com>
2012-10-19 00:51:38 +0200

    Fixed styling of code and posts.

--- NBlog.Web/Scripts/nblog.js
+++ NBlog.Web/Scripts/nblog.js
@@ -1,10 +1,41 @@
 ﻿/// <reference path="jquery-1.8.2.intellisense.js" />
-(function () {
-    var converter = Markdown.getSanitizingConverter();
-    var options = {
-        inputId: "Content"
-    }
-    var editor = new Markdown.Editor(converter, options);
-    editor.run();
-})();
+$(function() {
+    (function () {
+        var converter = Markdown.getSanitizingConverter();
+        var options = {
+            inputId: "Content"
+        }
+        var editor = new Markdown.Editor(converter, options);
+        editor.run();
+        $('#Content').keydown(function () {
+            var elem = $(this);
+            if(elem.timer) {
+                clearTimeout(elem.timer);
+            }
+            elem.timer = setTimeout('stylePreview()', 2000);
+        });
+        styleCode();
+    })();
+});
 
+function stylePreview() {
+    $(".wmd-preview pre").addClass("prettyprint");
+    $(".wmd-preview code").html(prettyPrintOne($(".wmd-preview code").html()));
+}
+
+function styleCode() {
+    if (typeof disableStyleCode != "undefined") {
+        return;
+    }
+
+    var a = false;
+
+    $("pre code").parent().each(function () {
+        if (!$(this).hasClass("prettyprint")) {
+            $(this).addClass("prettyprint");
+            a = true
+        }
+    });
+
+    if (a) { prettyPrint() }
+}
